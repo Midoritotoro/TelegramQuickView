6d5e9c20801f4e81b83cbee3aaf0532fe7f4a6ab
Midoritotoro <danya@USER1234>
2024-06-25 17:30:10 +0300

    change

--- TelegramQuickView/src/gui/core/ScrollBar.cpp
+++ TelegramQuickView/src/gui/core/ScrollBar.cpp
@@ -2,108 +2,53 @@
 
 #include "../media/ClickableLabel.h"
 
-
 ScrollBar::ScrollBar(QWidget* parent) :
 	QMainWindow(parent)
 {
 	CreateScrollBar();
 }
 
-void ScrollBar::CreateScrollBar() {
-    resize(600, 400);
-
+void ScrollBar::CreateScrollBar() 
+{
     int screenHeight = GetSystemMetrics(SM_CYSCREEN);
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+
     setGeometry(screenWidth - width(), 0, width(), screenHeight);
 
     QString currentPath = QCoreApplication::applicationDirPath();
     QDir assetsDir(currentPath + "/../../TelegramQuickView/assets/images");
     QString chatBackgroundPath = assetsDir.absolutePath() + "/chat_background.jpg";
-    setContentsMargins(0, 0, 0, 0);
-
-    QWidget* chatListWidget = new QWidget(this);
-    setCentralWidget(chatListWidget);
-    chatListWidget->setContentsMargins(0, 0, 0, 0);
     QString imagePath = "C:\\Users\\danya\\Downloads\\avatar.jpg";
-    QGridLayout* chatGridLayout = new QGridLayout(chatListWidget);
+
+    chatListWidget = new QWidget(this);
+
+    setCentralWidget(chatListWidget);
+
+    chatGridLayout = new QGridLayout(chatListWidget);
+    chatList = new QListWidget(chatListWidget);
+
+    chatListWidget->setContentsMargins(0, 0, 0, 0);
     chatGridLayout->setContentsMargins(0, 0, 0, 0);
     chatGridLayout->setSpacing(0);
 
-    QListWidget* chatList = new QListWidget(chatListWidget);
-    QListWidgetItem* chatItem1 = new QListWidgetItem(chatList);
-    QWidget* chatWidget1 = new QWidget();
-    QHBoxLayout* chatLayout1 = new QHBoxLayout(chatWidget1);
-
-    ClickableLabel* _IconChannelLabel1 = new ClickableLabel();
-    QLabel* nameLabel1 = new QLabel("ChatGPT4 | Midjourney fg g gg ggggggggggggg"); // Нужна обрезка текста при нехватке места
-    _IconChannelLabel1->setFixedSize(50, 50);
-    nameLabel1->setFixedWidth(width() / 3 - (chatLayout1->contentsMargins().left() + chatLayout1->contentsMargins().right()) - _IconChannelLabel1->width());
-    QFontMetrics metrics(nameLabel1->font());
-    QString elidedText = metrics.elidedText("ChatGPT4 | Midjourney fg g gg ggggggggggggg", Qt::ElideRight, nameLabel1->width());
-    nameLabel1->setText(elidedText);
-
-
-    QPixmap pixmap1(imagePath);
-    _IconChannelLabel1->setBackgroundRole(QPalette::Dark);
-    _IconChannelLabel1->setScaledContents(true);
-    _IconChannelLabel1->setPixmap(pixmap1);
-    _IconChannelLabel1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    _IconChannelLabel1->setStyleSheet("background-color: transparent");
-    _IconChannelLabel1->setAttribute(Qt::WA_NoSystemBackground);
-    _IconChannelLabel1->setCursor(Qt::PointingHandCursor);
-    QRegion region1(QRect(0, 0, _IconChannelLabel1->width() - _IconChannelLabel1->width() * 0.01, _IconChannelLabel1->height() - _IconChannelLabel1->height() * 0.01), QRegion::Ellipse);
-    _IconChannelLabel1->setMask(region1);
-
-    chatLayout1->addWidget(_IconChannelLabel1);
-    chatLayout1->addWidget(nameLabel1);
-
-    chatItem1->setSizeHint(QSize(width() / 3, height() / 18));
-    chatList->setItemWidget(chatItem1, chatWidget1);
-
-    QListWidgetItem* chatItem2 = new QListWidgetItem(chatList);
-    QWidget* chatWidget2 = new QWidget();
-    QHBoxLayout* chatLayout2 = new QHBoxLayout(chatWidget2);
-
-    ClickableLabel* _IconChannelLabel2 = new ClickableLabel();
-    QLabel* nameLabel2 = new QLabel("ChatGPT4 | Midjourney");
-    QPixmap pixmap(imagePath);
-    _IconChannelLabel2->setBackgroundRole(QPalette::Dark);
-    _IconChannelLabel2->setScaledContents(true);
-    _IconChannelLabel2->setPixmap(pixmap);
-    _IconChannelLabel2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    _IconChannelLabel2->setStyleSheet("background-color: transparent");
-    _IconChannelLabel2->setAttribute(Qt::WA_NoSystemBackground);
-    _IconChannelLabel2->setCursor(Qt::PointingHandCursor);
-    _IconChannelLabel2->setFixedSize(50, 50);
-    QRegion region2(QRect(0, 0, _IconChannelLabel2->width() - _IconChannelLabel2->width() * 0.01, _IconChannelLabel2->height() - _IconChannelLabel2->height() * 0.01), QRegion::Ellipse);
-    _IconChannelLabel2->setMask(region2);
-
-    chatLayout2->addWidget(_IconChannelLabel2);
-    chatLayout2->addWidget(nameLabel2);
-
-    chatItem2->setSizeHint(QSize(width() / 3, height() / 18));
-    chatList->setItemWidget(chatItem2, chatWidget2);
-
-    chatGridLayout->addWidget(chatList, 0, 0);
-
+    setContentsMargins(0, 0, 0, 0);
     QWidget* _MessageField = new QWidget();
     QVBoxLayout* messageLayout = new QVBoxLayout(_MessageField);
 
-    chatList->setStyleSheet("background-color: #17212b;");
+
     _MessageField->setStyleSheet("background-color: #0e1621;");
 
     setStyleSheet("background-color: #0e1621;");
-    QScrollArea* chatScrollArea = new QScrollArea(chatListWidget);
+    chatScrollArea = new QScrollArea(chatListWidget);
     chatScrollArea->setWidgetResizable(true);
-    chatScrollArea->setWidget(chatList);
     chatScrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    chatScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    chatScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-    QScrollArea* messageScrollArea = new QScrollArea(this);
+    messageScrollArea = new QScrollArea(this);
     messageScrollArea->setWidgetResizable(true);
     messageScrollArea->setWidget(_MessageField);
     messageScrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    messageScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    messageScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     _MessageField->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     chatGridLayout->addWidget(chatScrollArea, 0, 0);
@@ -113,4 +58,49 @@ void ScrollBar::CreateScrollBar() {
     chatScrollArea->setFixedWidth(width() / 3);
 
     chatListWidget->setLayout(chatGridLayout);
+
+    for (int i = 0; i < 100; i++)
+        createChat(imagePath, "ChatGPT | Midjourney            ");
+}
+
+
+void ScrollBar::adjustTextWidth(QLabel* label)
+{
+    QFontMetrics metrics(label->font());
+    QString elidedText = metrics.elidedText(label->text(), Qt::ElideRight, label->width());
+    label->setText(elidedText);
+}
+
+void ScrollBar::createChat(const QString imagePath, const QString chatName)
+{
+    QListWidgetItem* chatItem = new QListWidgetItem(chatList);
+    QWidget* chatWidget = new QWidget();
+    QHBoxLayout* chatLayout = new QHBoxLayout(chatWidget);
+    ClickableLabel* _IconChannelLabel = new ClickableLabel();
+    QLabel* chatNameLabel = new QLabel(chatName);
+    QPixmap pixmap1(imagePath);
+
+    chatItem->setSizeHint(QSize(width() / 3, height() / 18));
+    chatList->setItemWidget(chatItem, chatWidget);
+    chatList->setStyleSheet("background-color: #17212b;");
+
+    _IconChannelLabel->setFixedSize((height() / 18) * 0.8, (height() / 18) * 0.8);
+    chatNameLabel->setFixedWidth(width() / 3 - (chatLayout->contentsMargins().left() + chatLayout->contentsMargins().right()) - _IconChannelLabel->width());
+    adjustTextWidth(chatNameLabel);
+
+    _IconChannelLabel->setBackgroundRole(QPalette::Dark);
+    _IconChannelLabel->setScaledContents(true);
+    _IconChannelLabel->setPixmap(pixmap1);
+    _IconChannelLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    _IconChannelLabel->setStyleSheet("background-color: transparent");
+    _IconChannelLabel->setAttribute(Qt::WA_NoSystemBackground);
+    _IconChannelLabel->setCursor(Qt::PointingHandCursor);
+    QRegion region(QRect(0, 0, _IconChannelLabel->width() - _IconChannelLabel->width() * 0.01, _IconChannelLabel->height() - _IconChannelLabel->height() * 0.01), QRegion::Ellipse);
+    _IconChannelLabel->setMask(region);
+
+    chatLayout->addWidget(_IconChannelLabel);
+    chatLayout->addWidget(chatNameLabel);
+
+    chatGridLayout->addWidget(chatList,0, 0);
+    chatScrollArea->setWidget(chatList);
 }
--- TelegramQuickView/src/gui/core/ScrollBar.h
+++ TelegramQuickView/src/gui/core/ScrollBar.h
@@ -9,7 +9,13 @@ class ScrollBar: public QMainWindow
 {
 private:
 	Q_OBJECT
+	QWidget* chatListWidget = nullptr;
+	QGridLayout* chatGridLayout = nullptr;
+	QScrollArea* chatScrollArea = nullptr, * messageScrollArea = nullptr;
+	QListWidget* chatList = nullptr;
 public:
 	ScrollBar(QWidget* parent = nullptr);
 	void CreateScrollBar();
+	void adjustTextWidth(QLabel* label);
+	void createChat(const QString imagePath, const QString chatName);
 };
