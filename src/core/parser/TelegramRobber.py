11b4977e14db174fcc26d82cf3e0a59f56468f3a
Midoritotoro <danya20090504@gmail.com>
2024-07-09 01:02:55 +0300

    fix

--- src/core/parser/TelegramRobber.py
+++ src/core/parser/TelegramRobber.py
@@ -7,6 +7,7 @@ import asyncio
 from pathlib import Path
 from shutil import rmtree
 import json
+from os import listdir, rename
 
 channels = ["test329483", "testssss352"]
 
@@ -29,6 +30,7 @@ class Sleuth:
         self.codeHash = telegramCredentials.get("codeHash")
         
         self.__pathToAppRootDirectory = pathToAppRootDirectory
+        self.__pathToAppRootDirectoryContent = ""
         
         self.__client = TelegramClient('TelegramQuickView', self.api_id, self.api_hash, timeout=10)
         
@@ -39,14 +41,14 @@ class Sleuth:
             self.__pathToAppRootDirectoryContent = self.__pathToAppRootDirectory + f"/{username}/" + f"{self.postIndex}"
         else:
             self.__pathToAppRootDirectoryContent = self.__pathToAppRootDirectoryContent[:-1] + f"{self.postIndex}"
-            
+
         self.__download_paths = [
-            f"{self.__pathToAppRootDirectory}/Изображения",
-            f"{self.__pathToAppRootDirectory}/Видео",
-            f"{self.__pathToAppRootDirectory}/Аудио",
-            f"{self.__pathToAppRootDirectory}/Документы",
-            f"{self.__pathToAppRootDirectory}/Остальное",
-            f"{self.__pathToAppRootDirectory}/Текст"
+            f"{self.__pathToAppRootDirectoryContent}/Изображения",
+            f"{self.__pathToAppRootDirectoryContent}/Видео",
+            f"{self.__pathToAppRootDirectoryContent}/Аудио",
+            f"{self.__pathToAppRootDirectoryContent}/Документы",
+            f"{self.__pathToAppRootDirectoryContent}/Остальное",
+            f"{self.__pathToAppRootDirectoryContent}/Текст"
         ]
         
     def __getUserSettings(self) -> dict[str, str]:
@@ -54,37 +56,41 @@ class Sleuth:
             data = json.load(jsonFile)
         return data
 
-    async def __get_messages(self, username: str):
+    async def __get_messages(self, username: str, limit: int):
         self.postIndex = 0
         tasks = []
         export = []
         async for message in self.__client.iter_messages(
                 username,
-                limit = self.lastPostsCount
+                limit = limit
         ):
          
             if not message:
                 break
+            
+            print(self.__pathToAppRootDirectoryContent)
             
             if self.postIndex == 0:
                 await self.__updatePaths(True, username)
             else:
                 await self.__updatePaths(False)
 
+            print(self.__pathToAppRootDirectoryContent)
+            
             if message.sender is not None and message.text is not None:
+                await self.__check_download_path()
                 clean_message = message.text
-
+                export.append(asyncio.create_task(self.__export_to_txt(clean_message, f"{self.__download_paths[5]}/{self.postIndex}.txt")))
+                
                 if message.file is not None:
                     file_type = message.file.mime_type.split('/')[0]
-                    await self.__check_download_path()
                     download_path = await self.__get_download_path(file_type)
                     tasks.append(asyncio.create_task(message.download_media(file=download_path)))
-                    export.append(asyncio.create_task(self.__export_to_txt(clean_message, f"{self.__download_paths[5]}/{self.postIndex}.txt")))
 
-                if len(tasks) == self.lastPostsCount:
-                    await asyncio.gather(*tasks, *export)
-                    tasks.clear()
-                    export.clear()
+            if len(tasks) == limit:
+                await asyncio.gather(*tasks, *export)
+                tasks.clear()
+                export.clear()
 
     async def __export_to_txt(self, message: str, output_path: str) -> None:
         with open(output_path, "w", encoding="utf-8") as file:
@@ -105,6 +111,19 @@ class Sleuth:
             Path(self.__pathToAppRootDirectoryContent).mkdir(parents=True)
         for download_path in self.__download_paths:
             Path(download_path).mkdir(parents=True, exist_ok=True)
+        while True:
+            dirObjects = listdir(self.__pathToAppRootDirectoryContent.rsplit('/', 1)[0])
+            
+            if not (int(dirObjects[-1]) > self.lastPostsCount):
+                break
+            
+            rmtree(self.__pathToAppRootDirectoryContent.rsplit('/', 1)[0] + "/" + dirObjects[0])
+                
+        dirObjects = listdir(self.__pathToAppRootDirectoryContent.rsplit('/', 1)[0])
+        for index, obj in enumerate(dirObjects, start=1):
+            if obj != str(index):
+                rename(self.__pathToAppRootDirectoryContent.rsplit('/', 1)[0] + "/" + obj)
+        
 
     async def checkAndParseTelegramChannels(self) -> None:
         await self.__client.connect()
@@ -126,7 +145,7 @@ class Sleuth:
         if isinstance(channel_entity, InputPeerChannel):
             @self.__client.on(events.NewMessage(chats=channel_entity))
             async def handler(event):
-                await self.__get_messages(username)
+                await self.__get_messages(username, 1)
                 print(f"Новый пост в канале: {username}")
                 print(f"Текст: {event.message.text}")
             print(f"Создан обработчик для канала: {username}")
@@ -137,6 +156,6 @@ class Sleuth:
         self.__client.loop.run_until_complete(self.checkAndParseTelegramChannels())
   
 if __name__ == "__main__":
-   telegramParser = Sleuth("C:/Users/danya/AppData/Roaming/TelegramQuickView/userData.json", "D:/Media")
-   telegramParser.start()
+    telegramParser = Sleuth("C:/Users/danya/AppData/Roaming/TelegramQuickView/userData.json", "D:/Media")
+    telegramParser.start()
             
