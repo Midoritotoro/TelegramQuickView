c251ed284c6ca10d18e29f2dee1900c29e9621de
Midoritotoro <danya20090504@gmail.com>
2024-07-08 21:50:40 +0300

    fx

--- src/core/parser/tel.py
+++ src/core/parser/tel.py
@@ -1,56 +1,23 @@
 ﻿from telethon import TelegramClient, events
 from telethon.tl.types import InputPeerChannel, PeerChannel
 from telethon.errors import PeerFloodError
+import asyncio
 
-api_id = 13711370  # Замените на свой API ID
-api_hash = '019edf3f20c8460b741fb94114e6fec0'  # Замените на свой API Hash
-phone = '+375292384917'  # Замените на свой номер телефона
 
+api_id = 13711370
+api_hash = '019edf3f20c8460b741fb94114e6fec0'
+phone = '+375292384917'
+
+channels = ["test329483", "testssss352"]
 client = TelegramClient('TelegramQuickView', api_id, api_hash)
 
-# Словарь для хранения каналов и их ID
-channels = {}
-
-async def main():
-    await client.connect()
-    if not await client.is_user_authorized():
-        await client.send_code_request(phone)
-        await client.sign_in(phone, input('Введите код: '))
-
-    # Добавляем обработчик для новых сообщений
-    @client.on(events.NewMessage(chats='antifishechki'))
-    async def handler(event):
-        # Получаем имя канала из словаря
-        print(f"Новый пост в канале: 'antifishechki'")
-        print(f"Текст: {event.message.text}")
-
-    # Функция для добавления канала в список
-    async def add_channel(channel_name):
-        try:
-            channel = await client.get_input_entity(channel_name)
-            if isinstance(channel, InputPeerChannel):
-                channels[channel_name] = channel.channel_id
-                print(f"Канал {channel_name} добавлен!")
-            else:
-                print("Ошибка: Это не канал.")
-        except PeerFloodError:
-            print("Ошибка: Слишком много запросов. Попробуйте позже.")
-        except Exception as e:
-            print(f"Ошибка: {e}")
-
-    # Функция для удаления канала из списка
-    async def remove_channel(channel_name):
-        if channel_name in channels:
-            del channels[channel_name]
-            print(f"Канал {channel_name} удален.")
-        else:
-            print(f"Канал {channel_name} не найден.")
-
-    # Пример использования функций добавления и удаления каналов
-
-
-    # Запускаем слушателя событий
-    await client.run_until_disconnected()
-
-if __name__ == '__main__':
-    client.loop.run_until_complete(main())
+async def createChannelHandler(channel_name):
+    channel_entity = await client.get_input_entity(channel_name)
+    if isinstance(channel_entity, InputPeerChannel):
+        @client.on(events.NewMessage(chats=channel_entity))
+        async def handler(event):
+            print(f"Новый пост в канале: {channel_name}")
+            print(f"Текст: {event.message.text}")
+        print(f"Создан обработчик для канала: {channel_name}")
+    else:
+        print(f"Ошибка: {channel_name} - это не канал")
