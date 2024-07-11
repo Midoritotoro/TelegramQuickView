ba9e1f27e9d1abc5f97a11ce2e8801cd61e309ee
Midoritotoro <danya20090504@gmail.com>
2024-07-11 23:31:45 +0300

    fix

--- src/core/parser/main.cpp
+++ src/core/parser/main.cpp
@@ -1,9 +1,10 @@
 #include "PythonCaller.h"
+#include <Windows.h>
 
-
-int main() {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
 	const char* PyModuleName = "TelegramRobber";
 	PythonCaller* pythonCaller = new PythonCaller("D:\\TelegramQuickView\\src\\core\\parser\\");
+	FreeConsole();
 	pythonCaller->CallTelegramParseFunction(PyModuleName, "C:/Users/danya/AppData/Roaming/TelegramQuickView/userData.json", "D:/Media");
 	return 0;
 }
