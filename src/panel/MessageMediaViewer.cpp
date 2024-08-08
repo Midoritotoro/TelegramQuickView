daca754d9d8c2db8fe605b97a55eda3c04e9bed4
Midoritotoro <danya20090504@gmail.com>
2024-08-08 04:38:10 +0300

    fix

--- src/panel/MessageMediaViewer.cpp
+++ src/panel/MessageMediaViewer.cpp
@@ -11,20 +11,30 @@ MessageMediaViewer::MessageMediaViewer(QWidget* parent):
 	QWidget(parent)
 {
 	QToolButton* minimizeWindowButton = new QToolButton();
+	QToolButton* maximizeWindowButton = new QToolButton();
 	QToolButton* closeWindowButton = new QToolButton();
 
-	//setWindowFlag(Qt::FramelessWindowHint);
-	//setAttribute(Qt::WA_TranslucentBackground);
-	//setStyleSheet("color: rgba(1, 0, 0, 0)");
+	setWindowFlags(Qt::FramelessWindowHint);
+	setAttribute(Qt::WA_TranslucentBackground);
+	setStyleSheet("QWidget{\n "
+		"background-color: rgba(35,36,37, 0.5)\n"
+	"}");
 
 	QPixmap minPix = style()->standardPixmap(QStyle::SP_TitleBarMinButton);
 	QPixmap closePix = style()->standardPixmap(QStyle::SP_TitleBarCloseButton);
+	QPixmap maxPix = style()->standardPixmap(QStyle::SP_TitleBarMaxButton);
 
 	minimizeWindowButton->setIcon(minPix);
+	maximizeWindowButton->setIcon(maxPix);
 	closeWindowButton->setIcon(closePix);
 
 	minimizeWindowButton->setAttribute(Qt::WA_NoSystemBackground);
 	closeWindowButton->setAttribute(Qt::WA_NoSystemBackground);
+	maximizeWindowButton->setAttribute(Qt::WA_NoSystemBackground);
+
+	minimizeWindowButton->setStyleSheet("background-color: transparent;");
+	closeWindowButton->setStyleSheet("background-color: transparent;");
+	maximizeWindowButton->setStyleSheet("background-color: transparent;");
 
 	_grid = new QGridLayout(this);
 	_mediaPlayer = new MediaPlayer();
@@ -41,21 +51,24 @@ MessageMediaViewer::MessageMediaViewer(QWidget* parent):
 	toolLayout->setContentsMargins(0, 0, 0, 0);
 	toolLayout->setSpacing(0);
 
-	toolLayout->addWidget(minimizeWindowButton, 0, 0, 1, 1, Qt::AlignRight | Qt::AlignTop);
-	toolLayout->addWidget(closeWindowButton, 0, 1, 1, 1, Qt::AlignRight | Qt::AlignTop);
+	toolLayout->addWidget(minimizeWindowButton, 0, toolLayout->columnCount(), 1, 1, Qt::AlignRight | Qt::AlignTop);
+	toolLayout->addWidget(maximizeWindowButton, 0, toolLayout->columnCount(), 1, 1, Qt::AlignRight | Qt::AlignTop);
+	toolLayout->addWidget(closeWindowButton, 0, toolLayout->columnCount(), 1, 1, Qt::AlignRight | Qt::AlignTop);
 
 	toolLayout->setAlignment(Qt::AlignRight | Qt::AlignTop);
 	_grid->setVerticalSpacing(0);
-	//_grid->addWidget(toolWidget, _grid->rowCount(), 0, 1, 1);
+	_grid->addWidget(toolWidget, _grid->rowCount(), 0, 1, 1);
 	_grid->addWidget(_mediaPlayer, _grid->rowCount(), 0, 1, 1);
 
 	setContentsMargins(0, 0, 0, 0);
-	//setWindowFlag(Qt::SplashScreen);
 
 	_mediaPlayer->setVisible(false);
 
 	connect(minimizeWindowButton, &QToolButton::clicked, this, &QWidget::showMinimized);
 	connect(closeWindowButton, &QToolButton::clicked, this, &QWidget::close);
+	connect(maximizeWindowButton, &QToolButton::clicked, this, [this]() {
+		isFullScreen() ? showNormal() : showFullScreen();
+	});
 }
 
 void MessageMediaViewer::openMessageAttachment(MessageAttachment* messageAttachment) {
