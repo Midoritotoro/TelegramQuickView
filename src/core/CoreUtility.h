29b61f24f992137aaadaf9a8c9e49f5a11601ae3
Midoritotoro <danya20090504@gmail.com>
2024-12-18 22:13:59 +0300

    Fix

--- src/core/CoreUtility.h
+++ src/core/CoreUtility.h
@@ -17,7 +17,7 @@
 #include <ranges>
 #include <gsl/gsl>
 
-#include "Concepts.h"
+#include <__msvc_iter_core.hpp>
 
 using namespace std::ranges;
 
@@ -36,8 +36,7 @@ namespace core::utility {
 	struct plus
 	{
 		template<typename T, typename U>
-		constexpr auto operator()(T&& t, U&& u) const -> decltype((T&&)t + (U&&)u)
-		{
+		constexpr auto operator()(T&& t, U&& u) const -> decltype((T&&)t + (U&&)u) {
 			return (T&&)t + (U&&)u;
 		}
 		using is_transparent = void;
@@ -46,49 +45,43 @@ namespace core::utility {
 	struct identity
 	{
 		template<typename T>
-		constexpr T&& operator()(T&& t) const noexcept
-		{
+		constexpr T&& operator()(T&& t) const noexcept {
 			return (T&&)t;
 		}
 		using is_transparent = void;
 	};
 
-	//template <input_range _First, forward_range... _Rest>
-	//	requires (view<_First> && ... && view<_Rest>)
+	template <typename F, typename Type, typename Type2>
+	concept indirectly_binary_invocable = 
+		std::indirectly_readable<Type> &&
+		std::indirectly_readable<Type2> &&
+		std::copy_constructible<F>;
 
-	struct accumulate_fn
-	{
+	struct accumulate_fn {
 
+		template<typename I, typename S, typename T, 
+			typename Op = plus, typename P = identity>
 
-	/*	template <typename I>
-		concept InputIter = requires ()
-		template <I _First, typename S _Last, typename T _Init, typename Op = plus,
-			typename P = identity>
+		requires std::input_iterator<I>&& std::sentinel_for<S, I>     &&
+			indirectly_binary_invocable<Op, T*, std::projected<I, P>> &&
+			std::assignable_from<T&, std::indirect_result_t<Op&,
+				T*, std::projected<I, P>>>
 
-		requires std::sentinel_for<S, I> AND std::input_iterator<I> AND
-			std::indirectly_binary_invocable_<Op, T*, std::projected<I, P>> AND
-			std::assignable_from<T&, std::indirect_result_t<Op&, T*, std::projected<I, P>>>) */
-		template<typename I, typename S, typename T, typename Op = plus,
-			typename P = identity>
-		T operator()(I first, S last, T init, Op op = Op{},
-			P proj = P{}) const
-		{
+		T operator()(I first, S last, T init, Op op = Op{}, P proj = P{}) const {
 			for (; first != last; ++first)
-				init = invoke(op, init, invoke(proj, *first));
+				init = std::invoke(op, init, std::invoke(proj, *first));
 			return init;
 		}
 
 		template<typename Rng, typename T, typename Op = plus, typename P = identity>
-		
-		//concept Cn = (requires input_range<Rng> AND
-		//	indirectly_binary_invocable_<Op, T*, projected<iterator_t<Rng>, P>> AND
-		//	assignable_from<
-		//	T&, indirect_result_t<Op&, T*, projected<iterator_t<Rng>, P>>>);
+			requires 
+			input_range<Rng>														&&
+			indirectly_binary_invocable<Op, T*, std::projected<iterator_t<Rng>, P>> &&
+			std::assignable_from<T&, std::indirect_result_t<Op&, T*,
+				std::projected<iterator_t<Rng>, P>>>
 
-			T operator()(Rng&& rng, T init, Op op = Op{}, P proj = P{}) const
-		{
-			return (*this)(
-				begin(rng), end(rng), std::move(init), std::move(op), std::move(proj));
+			T operator()(Rng&& rng, T init, Op op = Op{}, P proj = P{}) const {
+				return (*this)(std::ranges::begin(rng), std::ranges::end(rng), std::move(init), std::move(op), std::move(proj));
 		}
 	};
 
