7fad9888f0c115be0301f8cdf66c5f442864ae6e
Midoritotoro <danya20090504@gmail.com>
2024-12-24 22:51:11 +0300

    Fix

---
+++ src/core/CoreConcurrent.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "concurrent/common/CommonConfig.h"
+
+#include "concurrent/common/CommonQueue.h"
+#include "concurrent/common/CommonSemaphore.h"
+
+#if defined USE_WINAPI
+#include "concurrent/winapi/WinapiAsync.h"
+#else // USE_WINAPI
+#error "Configuration is not supported."
+#endif // !USE_WINAPI
